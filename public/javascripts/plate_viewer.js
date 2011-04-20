72ad3e83b556cb997bf9ef507901a5d437f9a0db
Sean Dunn <sd9@sanger.ac.uk>
2011-04-20 09:59:48 +0100

    Upgraded jquery Added tooltip support Romoved last well from data for comparison Improved the well rendering

--- public/javascripts/plate_viewer.js
+++ public/javascripts/plate_viewer.js
@@ -1,9 +1,8 @@
 // *** PLATE VIEWER CODE ***
-$(function() {
+$(window).load(function() {
   
-  // Button
-  $("#button").button();
-  $("#radioset").buttonset();
+  // Control Buttons
+  $(".ui-buttonset").buttonset();
   
   // ========
   // = Well =
@@ -24,18 +23,74 @@ $(function() {
 
   window.WellView = Backbone.View.extend({
     
+    detailsTemplate: _.template($('#details-template').html()),
+    
     initialize: function() {
       _.bind(this, 'render');
       this.el = $('#well_' + this.model.get('map'));
       this.model.view = this;
+      this.setUpDetails();
+      this.setUpToolTip();
     },
     
     render: function() {
-      this.el.html(this.model.get('map'));
+    this.removeColours();
+    this.setValue(this.model.get('map'));
+    this.el.addClass('unknown-value');
+    },
+    
+    removeColours: function() {
+      this.el.removeClass('red').
+              removeClass('orange').
+              removeClass('green').
+              removeClass('unknown-value');
+    },
+    
+    setUpToolTip: function() {
+      this.el.attr('title',
+        'Sample: ' + this.model.get('sample_name') +
+        '<br/>Map: ' + this.model.get('map'));
+    },
+    
+    setUpDetails: function() {
+      debugger;
+      $('#container').add(
+        this.detailsTemplate({
+                  map: this.model.get('map'),
+          sample_name: this.model.get('sample_name')
+        })
+      );
+    },
+    
+    setValue: function(value) {
+      this.el.html((value ? value : "?"));
     },
     
     showConcentration: function() {
-      this.el.html(this.model.get('concentration'));
+      var conc = this.model.get('concentration');
+      
+      this.removeColours();
+      
+      if      (conc === null) { this.el.addClass('unknown-value'); }
+      else if (conc < 50.0)   { this.el.addClass('red');    }
+      else if (conc < 60.0)   { this.el.addClass('orange'); }
+      else if (conc > 60.0)   { this.el.addClass('green');  }
+      else                    { this.el.addClass('unknown-value'); }
+      
+      this.setValue(conc);
+    },
+    
+    showGelQc: function() {
+     var gelPass = this.model.get('gel_pass');
+     this.removeColours();
+     
+     if      (gelPass === null)      { this.el.addClass('unknown-value'); }
+     else if (gelPass == 'Fail')     { this.el.addClass("red"); }
+     else if (gelPass == 'Degraded') { this.el.addClass('orange'); }
+     else if (gelPass == 'OK')       { this.el.addClass("green"); }
+     else                            { this.el.addClass('unknown-value'); }
+     
+     this.setValue(gelPass);
     }
     
   });
@@ -60,26 +115,22 @@ $(function() {
   });
   
 
-  window.PlateView = Backbone.View.extend({
-    el: $('table.plate'),
+  window.PlateControlView = Backbone.View.extend({
+    el: $('#plate-control'),
     
     events: {
-      "click": "showConcentrations"
-    },
-    
-    addModeButtons: function() {
-      $(this.el).append(
-        '<input type="submit" value="Concentration" id= "conc" class="button concentration" name="show-concentration" />'
-      );
+      "click #showLocation": "showLocations",
+      "click #showConcentration": "showConcentrations",
+      "click #showGelQc": "showGelQc"
     },
     
     initialize: function() {
       _.bind(this, "render");
-      this.initializeWells();
+      this.initializeWellViews();
       this.showLocations();
     },
     
-    initializeWells: function() {
+    initializeWellViews: function() {
       this.model.wells.each(this.makeWellView);
     },
     
@@ -101,12 +152,29 @@ $(function() {
       this.render(
         function(well) { well.view.showConcentration(); }
       );
+    },
+    
+    showGelQc: function() {
+     this.render(
+       function(well) { well.view.showGelQc(); }
+     ); 
     }
   });
   
 
-  window.current_plate = new Plate(plate_json);
-  window.plateview     = new PlateView({model: window.current_plate});
+  window.current_plate    = new Plate(plate_json);
+  window.plateControlView = new PlateControlView({model: window.current_plate});
+  $('.well').tooltip({
+  
+   // use the built-in fadeIn/fadeOut effect
+    effect: "fade",
+    opacity: 1,
+    delay: 0,
+    predelay: 800
+  
+  });
+  
+  $(".well[rel]").overlay();
 
   
 });
